87b4d721d5235d52fc0c408fa6bb71354784b051
chen junheng <13688939119@163.com>
2023-11-17 23:57:08 +0800

    add shopping list

---
+++ index.js
@@ -0,0 +1,16 @@
+let shoppingList = [
+  "移动硬盘",
+  "4090显卡",
+  "按摩仪",
+  "爱奇艺会员",
+  "Niz 键盘",
+  "外星人电脑",
+  "女装(偷偷)",
+]
+
+function buy(list, goods) {
+  return goods && list.includes(goods) ? list.filter(v => v != goods) : "No buy"
+}
+
+let newShoppingList = buy(shoppingList, "Niz 键盘");
+console.log(newShoppingList);
